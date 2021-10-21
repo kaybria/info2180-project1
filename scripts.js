c5a3d46fcb67a6340a21eb9002fd5d56ac054017
Kali Soutar <kalisoutar@gmail.com>
2021-10-21 16:52:08 -0500

    Completed website

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-/* Add your JavaScript to this file */
+window.onload = function (){
+    let subscribeButton = document.querySelector('button');
+    let message = document.querySelector(".message");
+    let email = document.querySelector  ('input')
+subscribeButton.onclick = () =>{
+    if (email.value == ""){
+        message.innerHTML = " Please enter a valid email address"
+        return false
+    }else{
+        message.innerHTML = " Thank you! Your email address " +email.value+ " has been added to our mailing list!"
+        return false
+    
+    }   
+}
+}
